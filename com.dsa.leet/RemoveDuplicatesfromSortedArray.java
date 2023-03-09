eb2e11e29adae9d742d3bd04254dc870b8e77ed5
vinothnda <vinonada@gmail.com>
2023-03-09 16:23:04 +0530

    RemoveDuplicatesfromSortedArray fininised

--- com.dsa.leet/RemoveDuplicatesfromSortedArray.java
+++ com.dsa.leet/RemoveDuplicatesfromSortedArray.java
@@ -1,23 +1,25 @@
 package com.dsa.leet;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 //26
 public class RemoveDuplicatesfromSortedArray {
 
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[] {1,1,2}));
+        System.out.println(removeDuplicates(new int[] {1,2,3,4,5,6,6,6,7,7,7,8,8,9,9,9}));
     }
 
     static int removeDuplicates(int[] nums) {
-        HashMap<Integer,Integer> nums_arr= new HashMap<>();
         int count = 0;
         for(int i=0; i<nums.length; i++){
-            if(!nums_arr.containsKey(nums[i])){
-                nums_arr.put(nums[i],i);
-                count ++;
+            if(i<nums.length - 1 && nums[i] == nums[i+1]){
+                continue;
             }
+            nums[count] = nums[i];
+            count ++;
         }
+        System.out.println(Arrays.toString(nums));
         return count;
     }
 }
